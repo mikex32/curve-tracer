ed52daa7f4a0347fd0c27fdbed50fb393069702c
Michal Kalina <mike.mikex@gmail.com>
2014-05-08 14:19:50 +0200

    téměř hotový SW, chybí předělat nastavnování max napětí na max proud

--- sw/curve-tracer-gui/Forms/frmMain.Designer.cs
+++ sw/curve-tracer-gui/Forms/frmMain.Designer.cs
@@ -29,11 +29,333 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.port = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveChartAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFD = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // port
+            // 
+            this.port.BaudRate = 115200;
+            this.port.PortName = "COM10";
+            this.port.ReadBufferSize = 16384;
+            this.port.ReadTimeout = 500;
+            this.port.ReceivedBytesThreshold = 8;
+            this.port.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.port_DataReceived);
+            // 
+            // chart1
+            // 
+            chartArea6.AxisX.Crossing = -1.7976931348623157E+308D;
+            chartArea6.AxisX.Interval = 0.1D;
+            chartArea6.AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea6.AxisX.Maximum = 3D;
+            chartArea6.AxisX.Minimum = 0D;
+            chartArea6.AxisX.MinorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea6.AxisY.Interval = 0.1D;
+            chartArea6.AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea6.AxisY.Maximum = 3D;
+            chartArea6.AxisY.Minimum = 0D;
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(12, 24);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chart1.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Black};
+            this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.IsVisibleInLegend = false;
+            series6.Legend = "Legend1";
+            series6.Name = "va_char";
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(742, 408);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 464);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 1;
+            this.numericUpDown2.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.Location = new System.Drawing.Point(141, 471);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(834, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(760, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Max voltage:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(875, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "V";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 473);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Set max voltage:";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(483, 464);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 30);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Draw";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(366, 473);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = " ";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(598, 464);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 30);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Clear chart";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(766, 449);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Set COM";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(359, 489);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(94, 10);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10",
+            "COM11",
+            "COM12",
+            "COM13",
+            "COM14",
+            "COM15",
+            "COM16",
+            "COM17",
+            "COM18",
+            "COM19",
+            "COM20",
+            "COM21",
+            "COM22",
+            "COM23",
+            "COM24",
+            "COM25",
+            "COM26",
+            "COM27",
+            "COM28",
+            "COM29",
+            "COM30"});
+            this.comboBox1.Location = new System.Drawing.Point(781, 470);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.Text = "COM10";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(914, 24);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveChartAsToolStripMenuItem,
+            this.saveDataToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveChartAsToolStripMenuItem
+            // 
+            this.saveChartAsToolStripMenuItem.Name = "saveChartAsToolStripMenuItem";
+            this.saveChartAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveChartAsToolStripMenuItem.Text = "Save chart as...";
+            this.saveChartAsToolStripMenuItem.Click += new System.EventHandler(this.saveChartAsToolStripMenuItem_Click);
+            // 
+            // saveDataToolStripMenuItem
+            // 
+            this.saveDataToolStripMenuItem.Name = "saveDataToolStripMenuItem";
+            this.saveDataToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveDataToolStripMenuItem.Text = "Save data";
+            this.saveDataToolStripMenuItem.Click += new System.EventHandler(this.saveDataToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 516);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort port;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveChartAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFD;
     }
 }
-
--- sw/curve-tracer-gui/Forms/frmMain.cs
+++ sw/curve-tracer-gui/Forms/frmMain.cs
@@ -2,18 +2,210 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.Drawing.Design;
 
 namespace CurveTracerGui
 {
     public partial class frmMain : Form
-    {
+    {        
         public frmMain()
         {
             InitializeComponent();
         }
+
+        UInt16 id = 0;
+        double[] voltage = new double[4096];
+        double[] current = new double[4096];
+        double[] m_current = new double[4096];
+        decimal set_max_voltage = 0;
+        string max_voltage;
+        int send_max = 0;
+        byte[] buffer = new byte[2];
+        string send;
+
+
+
+        private UInt16 ReadU16(System.IO.Stream stm)
+        {
+            byte[] buffer = new byte[2];
+            stm.Read(buffer, 0, 2);
+            return (UInt16)(buffer[0] + buffer[1] * 256);
+        }
+
+        private void port_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            if (port.IsOpen)
+            {
+                progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Minimum = 0; }));
+                progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Maximum = send_max; }));
+                label5.Invoke(new MethodInvoker(delegate { label5.Text = "Recieving data..."; }));
+                button2.Invoke(new MethodInvoker(delegate { button2.Enabled = false; }));
+                while (port.BytesToRead > 1)
+                {
+                    m_current[id] = ReadU16(port.BaseStream);
+                    progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Increment(1); }));
+                    if (id >= send_max)
+                    {            
+                        id = 0;
+                        port.Close();
+                        label5.Invoke(new MethodInvoker(delegate { label5.Text = "Data recieved!"; }));
+                        button2.Invoke(new MethodInvoker(delegate { button2.Enabled = true; }));
+                        break;
+                    }
+                    id++;
+                }
+            }
+        
+        }
+       
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            set_max_voltage = numericUpDown2.Value;
+            send_max = (int)(((float)set_max_voltage - 0.017) / 0.000654);
+            send = send_max.ToString();
+            max_voltage = numericUpDown2.Value.ToString();
+            label1.Text = max_voltage;
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(0, 0);
+            if (send_max >= 4095)
+                send_max = 4095;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        { 
+            try
+            {
+                if (!port.IsOpen)
+                    port.Open();
+                if (port.IsOpen)
+                    port.Write(send);
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Device not connected! Make sure you are using proper COM port.","Error");
+            }
+
+
+            progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Value = 0; }));
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            set_max_voltage = numericUpDown2.Value;
+            send_max = (int)(((float)set_max_voltage - 0.0173) / 0.000654);
+            send = send_max.ToString();
+            max_voltage = numericUpDown2.Value.ToString();
+            label1.Text = max_voltage;
+            if (send_max >= 4095)
+                send_max = 4095;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < send_max + 1; i++)
+            {
+                if (i == 0)
+                {
+                    voltage[i] = 0;
+                    current[i] = 0;
+                }
+                else
+                {
+                    voltage[i] = ((i / 1.530) + 17.3) / 1000;
+                    current[i] = ((m_current[i] / 1.53) + 17.3) / 1000;
+                }
+            }
+            chart1.ChartAreas[0].AxisY.Maximum = current[send_max] + current[send_max] / 5;
+            chart1.ChartAreas[0].AxisX.Maximum = voltage[send_max] + voltage[send_max] / 5;
+
+            for (int i = 0; i < send_max + 1; i++)
+            {
+                chart1.Series[0].Points.AddXY(voltage[i], current[i]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(0, 0);
+            chart1.ChartAreas[0].AxisX.Maximum = 3;
+            chart1.ChartAreas[0].AxisY.Maximum = 3;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            port.PortName = comboBox1.Text;
+        }
+
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are u sure you want to Exit?", "Exit", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+
+                System.Windows.Forms.Application.Exit(); 
+
+            }
+        }
+
+        private void saveChartAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string saved_img = " ";
+
+            saveFD.InitialDirectory = "C:";
+            saveFD.Title = "Save chart image";
+            saveFD.FileName = " ";
+            saveFD.Filter = "Image files|*.jpg|All Files|*.*";
+
+            if (saveFD.ShowDialog() != DialogResult.Cancel)
+            {
+                saved_img = saveFD.FileName;
+                chart1.SaveImage(saved_img, ImageFormat.Jpeg);
+                MessageBox.Show("Successfully saved", "Chart");
+            }
+        }
+
+        private void saveDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int i;
+            string saved_data = " ";
+            string[] data = new string[4097];
+            int help = 0;
+
+            saveFD.InitialDirectory = "C:";
+            saveFD.Title = "Save chart image";
+            saveFD.FileName = " ";
+            saveFD.Filter = "Text files|*.txt|All Files|*.*";
+
+            if (saveFD.ShowDialog() != DialogResult.Cancel)
+            {
+                saved_data = saveFD.FileName;
+                data[0] = " U [V]     I [mA]";
+                for (i = 1; i < send_max; i++)
+                {
+                    data[i] = voltage[i - 1].ToString("N5") +"   "+ current[i - 1].ToString("N5");
+                }
+
+                File.WriteAllLines(saved_data,data,System.Text.Encoding.Default);
+                MessageBox.Show("Successfully saved", "Data");
+            }
+
+        }
+
+
+        
     }
 }
